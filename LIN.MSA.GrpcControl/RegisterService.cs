784e6e24563d379ef91ab7e215f318d114f33511
wenlong lin <linwenlong@rd.keytop.com.cn>
2020-07-09 18:01:50 +0800

    注册方法

--- LIN.MSA.GrpcControl/RegisterService.cs
+++ LIN.MSA.GrpcControl/RegisterService.cs
@@ -1,13 +1,28 @@
-﻿using LIN.MSA.GrpcService;
+﻿using Grpc.Core;
+using LIN.MSA.GrpcService;
 using System;
 
 namespace LIN.MSA.GrpcControl
 {
     public class RegisterService
     {
-        public static void Register()
+        public static string Register(string serAddress,string req)
         {
+            var channel = new Channel(serAddress, ChannelCredentials.Insecure);
 
+            try
+            {
+                var client = new PlatCenter.PlatCenterClient(channel);
+
+                var response = client.Register(new RegisterRequest { Data = req });
+
+                return response.Message;
+
+            }
+            catch (Exception)
+            {
+                return "";
+            }
         }
     }
 }
